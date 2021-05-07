322cb76e535d8c989981b2804a1666782c1ad109
emoneva <emoneva@aivo.co>
2021-05-07 16:11:09 -0300

    before presentation

---
+++ backend/routes/productRoute.js
@@ -0,0 +1,33 @@
+import express from 'express';
+import { Mongoose } from 'mongoose';
+import Product from '../models/productModel';
+import { getToken } from '../utils';
+
+const router = express.Router();
+
+router.get("/api/products", async(req, res) => {
+    const products = await Product.find({});
+    res.send(products);
+});
+
+router.post("/api/products", async(req, res) => {
+    const product = new Product({
+        name: req.body.name,
+        category: req.body.category,
+        image: req.body.image,
+        price: req.body.price,
+        brand: req.body.brand,
+        rating: req.body.rating,
+        numReviews: req.body.numReviews,
+        countInStock: req.body.countInStock,
+        isItNew: req.body.isItNew,
+        cubicCentimeters: req.body.cubicCentimeters,
+    });
+    const newProduct = await product.save();
+    if (newProduct){
+        return res.status(201).send({message: 'New product created', data: newProduct});
+    }
+    return res.status(500).send({message: 'Error in creating product'})
+})
+
+export default router;
--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,7 @@ import config from './config';
 import bodyParser from "body-parser";
 import mongoose from 'mongoose';
 import userRoute from './routes/userRoute';
-
+import productRoute from './routes/productRoute';
 dotenv.config();
 
 const mongodbUrl = "mongodb+srv://dbecomerce:123Mongo!!@cluster0.hobus.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
@@ -17,10 +17,11 @@ mongoose.connect(mongodbUrl, {
 }).catch(error=> console.log(error.reason));
 
 const app = express();
-
+var fs = require("fs");
 app.use(bodyParser.json());
 
 app.use("/api/users", userRoute);
+app.use("/api/products", productRoute);
 
 app.get("/api/products/:id", (req, res) => {
     const productId = req.params.id;
@@ -34,5 +35,28 @@ app.get("/api/products/:id", (req, res) => {
 app.get("/api/products", (req, res) => {
     res.send(data.products);
 });
+
+app.post("/api/products", (req, res) => {
+    var info = Object.values(req.body);
+    var da = Object.values(data.products);
+    da.push(info)
+    res.send(da);
+});
+ 
+app.delete("/api/products/remove/:id", (req, res) => {
+    const productId = req.params.id;
+    function deleteById(val) {
+        for(var f in data.products) {
+            if(data.products[f]['_id'] == val) {
+                delete data.products[f];
+            }
+        }
+    }
+    deleteById(productId)
+    var da = Object.values(data.products);
+    res.send(da);
+    
+});
+
 
 app.listen(5000, ()=> {console.log("Server started at http://localhost:5000") });
